c8b9a2853cbb8bd65685c51b93fabccb15069b1a
snipe <snipe@snipe.net>
2015-08-12 13:13:35 -0700

    Poking and hoping

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -5,6 +5,3 @@ $I->am('Website Visitor');
 $I->wantTo('ensure that frontpage loads without errors');
 $I->amGoingTo('go to the homepage');
 $I->lookForwardTo('logging in');
-$I->amOnPage('/');
-$I->seeElement('input[type=username]');
-$I->seeElement('input[type=password]');
