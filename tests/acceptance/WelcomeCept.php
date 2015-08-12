e906d4daba8eb12be8b615dfadcedc3ea19eb58f
snipe <snipe@snipe.net>
2015-08-12 13:10:50 -0700

    I cannot imagine that this will help, but whatever

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -4,7 +4,7 @@ $I = new AcceptanceTester($scenario);
 $I->am('Website Visitor');
 $I->wantTo('ensure that frontpage loads without errors');
 $I->amGoingTo('go to the homepage');
-$I->lookForwardTo('log in');
+$I->lookForwardTo('logging in');
 $I->amOnPage('/');
 $I->seeElement('input[type=username]');
 $I->seeElement('input[type=password]');
